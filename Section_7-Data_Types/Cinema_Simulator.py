3d3d5f797936650a643a74004e19693a2af025bd
drodriguez32 <d.rodriguez32@uniandes.edu.co>
2018-01-30 15:34:36 -0500

    Finish section 7
    Finishing cinema proyect for section 7

--- Section_7-Data_Types/Cinema_Simulator.py
+++ Section_7-Data_Types/Cinema_Simulator.py
@@ -2,10 +2,31 @@
 
 #create dictionary with Films:[Age,tickets]
 films = {
-        "Finding Dory":[3,5]
-        "Bourne":[18,5]
-        "Tarzan":[15,5]
+        "Finding Dory":[3,5],
+        "Bourne":[18,5],
+        "Tarzan":[15,5],
         "Ghost Busters":[12,5]
         }
 
+    
 choice = input("What film would you like to watch?: ").strip().title()
+
+if choice in films:
+    age = int(input("How old are you?: ").strip())
+    
+    # check user age
+    if age >= films[choice][0]:
+        num_seats = films[choice][1]
+        
+        # check enough seats
+        if num_seats > 0:
+            print("Enjoy the film!")
+            films[choice][1] = films[choice][1] - 1
+            
+        else:
+            print("Sorry, we are sold out")
+        
+    else:
+        print("You are too young to watch that film")
+else:
+    print("We don't have that film...") 
