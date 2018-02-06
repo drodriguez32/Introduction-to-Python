e61a6e19e6559510d825d307f2ad44bafdcaf030
drodriguez32 <d.rodriguez32@uniandes.edu.co>
2018-02-05 23:22:29 -0500

    Section 9 - Functions
    Programmed a game of tic tac toe

---
+++ Section_9-Functions/game.py
@@ -0,0 +1,73 @@
+board = [" " for i in range(9)]
+
+def print_board():
+    row1 = "| {} | {} | {} |".format(board[0],board[1],board[2])
+    row2 = "| {} | {} | {} |".format(board[3],board[4],board[5])
+    row3 = "| {} | {} | {} |".format(board[6],board[7],board[8])
+    
+    print("\n", row1, "\n", row2, "\n", row3, "\n")
+    
+def player_move(icon):
+    if icon == "X":
+        number = 1
+    elif icon == "O":
+        number = 2
+        
+    print ("Your turn player {}".format(number))
+    
+    choice = int(input("Enter your move (1-9): ").strip())
+    
+    if board[choice - 1] == " ":
+        board[choice - 1] = icon
+    else:
+        print("\n","That place is taken")
+        choice = int(input("Enter your move (1-9): ").strip())
+        board[choice - 1] = icon
+
+def is_victory(icon):
+    if(board[0] == icon and board[1] == icon and board[2]==icon) or \
+      (board[1] == icon and board[2] == icon and board[3]==icon) or \
+      (board[4] == icon and board[5] == icon and board[6]==icon) or \
+      (board[0] == icon and board[3] == icon and board[6]==icon) or \
+      (board[1] == icon and board[4] == icon and board[7]==icon) or \
+      (board[2] == icon and board[5] == icon and board[8]==icon) or \
+      (board[0] == icon and board[4] == icon and board[8]==icon) or \
+      (board[2] == icon and board[4] == icon and board[6]==icon):
+          return True
+    else:
+          return False
+    
+def is_draw():
+    if " " not in board:
+        return True
+    else:
+        return False
+
+
+while True: 
+    print_board()
+    player_move("X")
+    print_board()
+    
+    if is_victory("X"):
+        print("X Wins! Congratulations!")
+        break
+        print_board()
+    elif is_draw():
+        print("Its a draw! No one wins.")
+        break
+        print_board()
+        
+    player_move("O")
+    
+    if is_victory("O"):
+        print("O Wins! Congratulations!")
+        break
+        print_board()
+        
+    elif is_draw():
+        print("Its a draw! No one wins.")
+        break
+        print_board()
+        
+    
